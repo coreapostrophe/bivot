b557e4d3d08cfdff3d10d973d8220eda0ac0e62d
Jose Gabrielle Rivera <27613092+jgarivera@users.noreply.github.com>
2025-09-13 21:46:47 +0800

    feat: restructure server entrypoint to use new command registry

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,13 @@
-import { AutoRouter } from 'itty-router';
+import { AutoRouter, IRequest } from 'itty-router';
+import { verifyKey } from 'discord-interactions';
 import {
+  APIChatInputApplicationCommandInteraction,
+  APIInteraction,
   InteractionResponseType,
   InteractionType,
-  verifyKey,
-  InteractionResponseFlags,
-} from 'discord-interactions';
-import { AWW_COMMAND, INVITE_COMMAND } from './commands.js';
-import { getCuteUrl } from './reddit.js';
-
-interface DiscordInteraction {
-  type: InteractionType;
-  data?: {
-    name: string;
-  };
-}
-
-interface DiscordRequest {
-  method: string;
-  url: URL;
-  headers: Headers;
-  text(): Promise<string>;
-}
-
-interface Environment {
-  DISCORD_APPLICATION_ID: string;
-  DISCORD_PUBLIC_KEY: string;
-}
+} from 'discord-api-types/v10';
+import { CommandRegistry } from './command-registry';
+import { REST } from '@discordjs/rest';
 
 class JsonResponse extends Response {
   constructor(body: any, init?: ResponseInit) {
@@ -41,11 +23,13 @@ class JsonResponse extends Response {
 
 const router = AutoRouter();
 
-router.get('/', (request: DiscordRequest, env: Environment) => {
+const commandRegistry = new CommandRegistry();
+
+router.get('/', (request: IRequest, env: Env) => {
   return new Response(`👋 ${env.DISCORD_APPLICATION_ID}`);
 });
 
-router.post('/', async (request: DiscordRequest, env: Environment) => {
+router.post('/', async (request: IRequest, env: Env) => {
   const { isValid, interaction } = await server.verifyDiscordRequest(
     request,
     env,
@@ -54,48 +38,48 @@ router.post('/', async (request: DiscordRequest, env: Environment) => {
     return new Response('Bad request signature.', { status: 401 });
   }
 
-  if (interaction.type === InteractionType.PING) {
+  if (interaction.type === InteractionType.Ping) {
     return new JsonResponse({
-      type: InteractionResponseType.PONG,
+      type: InteractionResponseType.Pong,
     });
   }
 
-  if (interaction.type === InteractionType.APPLICATION_COMMAND) {
-    switch (interaction.data?.name.toLowerCase()) {
-      case AWW_COMMAND.name.toLowerCase(): {
-        const cuteUrl = await getCuteUrl();
-        return new JsonResponse({
-          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-          data: {
-            content: cuteUrl,
-          },
-        });
-      }
-      case INVITE_COMMAND.name.toLowerCase(): {
-        const applicationId = env.DISCORD_APPLICATION_ID;
-        const INVITE_URL = `https://discord.com/oauth2/authorize?client_id=${applicationId}&scope=applications.commands`;
-        return new JsonResponse({
-          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-          data: {
-            content: INVITE_URL,
-            flags: InteractionResponseFlags.EPHEMERAL,
-          },
-        });
-      }
-      default:
-        return new JsonResponse({ error: 'Unknown Type' }, { status: 400 });
-    }
+  if (interaction.type !== InteractionType.ApplicationCommand) {
+    return;
   }
 
-  console.error('Unknown Type');
-  return new JsonResponse({ error: 'Unknown Type' }, { status: 400 });
+  const name = interaction.data?.name.toLowerCase();
+
+  if (!name) {
+    return;
+  }
+
+  const discordRESTClient = new REST({ version: '10' }).setToken(
+    env.DISCORD_TOKEN,
+  );
+
+  const response = await commandRegistry.handle(
+    name,
+    interaction as APIChatInputApplicationCommandInteraction,
+    discordRESTClient,
+  );
+
+  if (!response) {
+    return new JsonResponse({ error: 'Unknown Type' }, { status: 400 });
+  }
+
+  return new JsonResponse({
+    type: response.type,
+    data: response.data,
+  });
 });
+
 router.all('*', () => new Response('Not Found.', { status: 404 }));
 
 async function verifyDiscordRequest(
-  request: DiscordRequest,
-  env: Environment,
-): Promise<{ isValid: boolean; interaction?: DiscordInteraction }> {
+  request: IRequest,
+  env: Env,
+): Promise<{ isValid: boolean; interaction?: APIInteraction }> {
   const signature = request.headers.get('x-signature-ed25519');
   const timestamp = request.headers.get('x-signature-timestamp');
   const body = await request.text();
